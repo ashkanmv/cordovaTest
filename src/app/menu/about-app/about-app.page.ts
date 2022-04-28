c4d1b71b9ae2e71490aed8389684e2d5f2142b92
a.mirzaei <ashkanmv@gmail.com>
2022-04-28 17:41:46 +0430

    about app finished

--- src/app/menu/about-app/about-app.page.ts
+++ src/app/menu/about-app/about-app.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-app',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutAppPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  backButton(){
+    this.router.navigate(['/'])
   }
 
 }
