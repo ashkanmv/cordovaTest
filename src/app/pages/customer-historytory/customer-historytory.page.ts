63b574ecca01c8fb85cadd6ed0d6524e86b6914d
Ashkan MV <ashkanmv@gmail.com>
2022-05-14 19:49:37 +0430

    customer history header

--- src/app/pages/customer-historytory/customer-historytory.page.ts
+++ src/app/pages/customer-historytory/customer-historytory.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-customer-historytory',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerHistorytoryPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
-
+  backButton(){
+    this.router.navigate(['/'])
+  }
 }
