a6c7e0bdfd3da08e38d0f7910a923a77100af8d1
a.mirzaei <ashkanmv@gmail.com>
2022-05-27 20:53:23 +0430

    language

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Data } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Language, LoginResponse } from 'src/app/shared/common';
+import { LanguageService } from 'src/app/shared/language.service';
+import { SharedService } from 'src/app/shared/shared.service';
+import { StorageService } from 'src/app/shared/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -8,20 +14,42 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder,) { }
+  isLoading = false;
+
+  public get language(): Language {
+    return this.languageService.language;
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private storageService: StorageService,
+    private sharedService: SharedService,
+    private languageService: LanguageService
+  ) { }
 
   ngOnInit() {
     this.loadForm();
   }
 
-  loadForm(){
+  loadForm() {
     this.form = this.formBuilder.group({
       'Username': [null, [Validators.required]],
-      'Password': [null, [Validators.required]]
+      'Password': [null, [Validators.required]],
+      'RememberMe': [null],
     });
   }
 
-  onSubmit(){
+  onSubmit() {
+    this.isLoading = true;
+    this.authService.getAccess(this.form.value.Username, this.form.value.Password, null)
+      .subscribe((res: LoginResponse) => {
+        this.isLoading = false;
+        // if (res.hasbusinesserror == 1 && res.Message == "UserPassIncorrect") 
+        //   this.sharedService.toast('danger',);
+        if (this.form.value.RememberMe)
+          this.storageService.set('RememberUser', true);
 
+      }, () => this.isLoading = false)
   }
 }
--- src/app/shared/common.ts
+++ src/app/shared/common.ts
@@ -24,3 +24,97 @@ export interface Marker {
   longitude: number;
   description?: string;
 }
+
+export interface LoginResponse {
+  route_name: string;
+  route_code: string;
+  FullName: string;
+  user_id: string;
+  Equivalent_User_ID: string;
+  userType: number;
+  access: Access[];
+  hasbusinesserror: number;
+  Message: string;
+}
+
+export interface Access {
+  name: string;
+}
+
+
+export class Language {
+  Customer_History: {
+    TitLe: string;
+  }
+  Questionnaire: {
+    "Title": string;
+    "Input-select-DC": string;
+    "Input-select-route": string;
+    "Input-select-routeDaily": string;
+    "Input-select-customer": string;
+    "Input-select-customer-Category": string;
+    "Input-text-store": string;
+    "Input-text-customer-name": string;
+    "Input-text-address": string;
+    "Radio-input-breakfast": string;
+    "Radio-input-breakfast-yes": string;
+    "Radio-input-breakfast-no": string;
+    "Submit-button": string;
+  }
+  Gps_Tracking: {
+    "Title": string;
+    "Description": string;
+    "Planned(Not_Invoiced_Yet)": string;
+    "Planned&Invoiced": string;
+    "OOP_invoiced": string;
+    "Visited_Not_Buy": string;
+    "SR": string;
+    "TRUCK": string;
+    "Input-select-date": string;
+    "Input-select-RSM": string;
+    "Input-select-ASM": string;
+    "Input-select-SSV": string;
+    "Input-select-SR": string;
+    "Radio-input-show-truck": string;
+    "Radio-input-show-SR": string;
+  }
+  Customer_nearby: {
+    "Title": string;
+    "Description": string;
+    "Normal_Customer": string;
+    "Promotion_Customer": string;
+    "PPED_>_5%": string;
+    "NOT_Buy_2_Weeks": string;
+    "Not_Buy_2_Weeks_&_PPED_>_5%": string;
+    "TRUCK": string;
+    "Input-select-date": string;
+    "Input-select-RSM": string;
+    "Input-select-ASM": string;
+    "Input-select-SSV": string;
+    "Input-select-SR": string;
+    "Radio-input-show-truck": string;
+    "Radio-input-show-SR": string;
+  }
+  Score_Card: {
+    "Title": string;
+    "Segment-category": string;
+    "Segment-channel": string;
+    "Segment-category%": string;
+    "Segment-channel%": string;
+    "Radio-input-sales": string;
+    "Radio-input-PPED": string;
+  }
+  login: {
+    "title": string;
+    "username": string;
+    "password": string;
+    "submit": string;
+    "UserPassIncorrect": string;
+    "RememberMe": string;
+  }
+}
+
+export enum Languages {
+  EN,
+  FA
+}
