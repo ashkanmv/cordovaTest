504d83c29cd9695d4318374247bf9193e708afb1
Ashkan MV <ashkanmv@gmail.com>
2022-05-28 22:08:08 +0430

    x

--- src/app/pages/questionnaire/questionnaire.page.ts
+++ src/app/pages/questionnaire/questionnaire.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Data, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 import { Cities, Customer } from 'src/app/shared/common';
+import { StorageService } from 'src/app/shared/storage.service';
+import { QuestionnaireService } from './questionnaire.service';
 
 @Component({
   selector: 'app-questionnaire',
@@ -8,19 +13,139 @@ import { Cities, Customer } from 'src/app/shared/common';
   styleUrls: ['./questionnaire.page.scss'],
 })
 export class QuestionnairePage implements OnInit {
+  form: FormGroup;
   cities: Cities[] = [];
   routes: { routename: string }[] = [];
   customers: Customer[] = [];
+  routedailys: any[] = [];
+  userId: string;
+  customerNumber: string;
+  typeSubscription: Subscription;
+  kgqtySubscription: Subscription;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private questionnaireService: QuestionnaireService,
+    private storageService: StorageService,
+    private loadingCtrl: LoadingController
+  ) {}
 
-  public get user_id(): string {
-    return
+  ngOnInit() {
+    this.loadForm();
+    this.getDataFromStorage();
   }
-
-  constructor(private router: Router) { }
-
-  ngOnInit() { }
   backButton() {
     this.router.navigate(['/']);
   }
-  
+
+  loadForm() {
+    this.form = this.formBuilder.group({
+      DC: [null],
+      Route: [null],
+      Customer: [null],
+      QuestionCategory: [null],
+      RouteDaily: [null],
+    });
+    // this.typeSubscription = this.form.controls['type'].valueChanges.subscribe(
+    //   (v) => this.typeChanged(v)
+    // );
+    // this.kgqtySubscription = this.form.controls['kgqty'].valueChanges.subscribe(
+    //   (v) => this.kgqtyChanged(v)
+    // );
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  getDataFromStorage() {
+    this.storageService.get('user_id').then((userId) => {
+      this.userId = userId;
+    });
+
+    this.storageService
+      .get('Customer_Number')
+      .then((customerNumber: string) => {
+        if (!customerNumber) this.get_cities();
+        else this.open_OtherForm(customerNumber);
+      });
+  }
+
+  async get_cities() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+    });
+    await loading.present();
+    this.questionnaireService.getCityByUserId(this.userId).subscribe(
+      (cities: Cities[]) => {
+        this.cities = cities;
+        if (cities.length) {
+          this.patchValue('DC', cities[0].City);
+          this.selectCity(cities[0].City);
+        }
+        loading.dismiss();
+      },
+      () => loading.dismiss()
+    );
+  }
+
+  async selectCity(value: string | any) {
+    this.routes = [];
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+    });
+    await loading.present();
+    console.log(typeof value);
+
+    let cityName = typeof value == 'string' ? value : value.detail.value;
+    this.questionnaireService.getRouteByUserId(cityName, this.userId).subscribe(
+      (routes: { routename: string }[]) => {
+        this.routes = routes;
+        console.log(routes);
+        loading.dismiss();
+      },
+      () => loading.dismiss()
+    );
+  }
+
+  async selectRoute(routeName: string) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+    });
+    await loading.present();
+    this.questionnaireService
+      .routedailySelectByRouteByUser(routeName, this.userId)
+      .subscribe(
+        (data: Data[]) => {
+          console.log(data);
+          this.routedailys = data;
+          loading.dismiss();
+        },
+        () => loading.dismiss()
+      );
+  }
+
+  async open_OtherForm(CustomerNumber: string) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+    });
+    await loading.present();
+    this.questionnaireService.getCustomersByNumber('21001009').subscribe(
+      (data: Data) => {
+        console.log(data);
+        loading.dismiss();
+      },
+      () => loading.dismiss()
+    );
+  }
+
+  typeChanged(value: string) {}
+
+  kgqtyChanged(value: string) {}
+
+  onSubmit() {}
+
+  patchValue(controller: string, value: any) {
+    this.form.patchValue({ [controller]: value });
+  }
 }
---
+++ src/app/pages/questionnaire/questionnaire.service.ts
@@ -0,0 +1,117 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuestionnaireService {
+  private questionUrl = environment.BaseURL + '/api/v1/questions';
+  private answerUrl = environment.BaseURL + '/api/v1/answers';
+  private customerUrl = environment.BaseURL + '/api/v1/questions/findshop';
+  private customerbynum = environment.BaseURL + '/api/v1/customers/bynumber'; //2AF-960327
+  private cityUrl = environment.BaseURL + '/api/v1/cities';
+  private routeUrl = environment.BaseURL + '/api/v1/routes';
+  private customerUrlR = environment.BaseURL + '/api/v1/customers';
+  private routedaily = environment.BaseURL + '/api/v1/routes/routedaily';
+  private byrouteidstep =
+    environment.BaseURL + '/api/v1/customers/byrouteidstep';
+  private questioncatbyuserid =
+    environment.BaseURL + '/api/v1/questions/questioncatbyuserid';
+  private questionbycat =
+    environment.BaseURL + '/api/v1/questions/questionbycat';
+  private questioncitybyuser =
+    environment.BaseURL + '/api/v1/cities/questioncitybyuser';
+  private routebyuser = environment.BaseURL + '/api/v1/routes/routebyuser';
+  private routedailybyuser =
+    environment.BaseURL + '/api/v1/routes/routedailybyuser';
+
+  constructor(private http: HttpClient) {}
+
+  getQuestions(language: string) {
+    let params = new HttpParams();
+    params = params.append('language', language);
+    return this.http.get(this.questionUrl, { params });
+  }
+  getQuestionsByCat(language: string, Category: string) {
+    let params = new HttpParams();
+    params = params.append('language', language);
+    params = params.append('CatID', Category);
+    return this.http.get(this.questionbycat, { params });
+  }
+  getAnswers() {
+    return this.http.get(this.answerUrl);
+  }
+
+  getCities() {
+    return this.http.get(this.cityUrl);
+  }
+
+  getCityByUserId(userid: string) {
+    let params = new HttpParams();
+    params = params.append('userid', userid);
+    return this.http.get(this.questioncitybyuser, { params });
+  }
+  getRouteByUserId(city: string, userid: string) {
+    let params = new HttpParams();
+    params = params.append('city', city);
+    params = params.append('userid', userid);
+    return this.http.get(this.routebyuser, { params });
+  }
+  getRoutesByCity(city: string) {
+    let params = new HttpParams();
+    params = params.append('city', city);
+    return this.http.get(this.routeUrl, { params });
+  }
+
+  getCustomersByRoute(route: string) {
+    let params = new HttpParams();
+    params = params.append('route', route);
+    return this.http.get(this.customerUrlR, { params });
+  }
+  getCustomersByRouteIdStep(routeid: string) {
+    let params = new HttpParams();
+    params = params.append('RouteID', routeid);
+    return this.http.get(this.byrouteidstep, { params });
+  }
+  getQuestionCatsByUserId(userid: string) {
+    let params = new HttpParams();
+    params = params.append('UserID', userid);
+    return this.http.get(this.questioncatbyuserid, { params });
+  }
+
+  routedailySelectByRoute(route: string) {
+    let params = new HttpParams();
+    params = params.append('route', route);
+    return this.http.get(this.routedaily, { params });
+  }
+
+  routedailySelectByRouteByUser(route: string, userid: string) {
+    let params = new HttpParams();
+    params = params.append('route', route);
+    params = params.append('userid', userid);
+    return this.http.get(this.routedailybyuser, { params });
+  }
+  getCustomers(lat: any, lng: any) {
+    let params = new HttpParams();
+    params = params.append('lat', lat);
+    params = params.append('lng', lng);
+    return this.http.get(this.customerUrl, { params });
+  }
+  getCustomersByNumber(number: string) {
+    let params = new HttpParams();
+    params = params.append('number', number);
+    return this.http.get(this.customerbynum, { params });
+  }
+  // private handleError(error: any) {
+  //   // In a real world app, we might use a remote logging infrastructure
+  //   // We'd also dig deeper into the error to get a better message
+  //   let errMsg = error.message
+  //     ? error.message
+  //     : error.status
+  //     ? `${error.status} - ${error.statusText}`
+  //     : 'Server error';
+  //   console.error(errMsg); // log to console instead
+  //   return Observable.throw(errMsg);
+  // }
+}
