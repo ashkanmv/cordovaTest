81fd527377fd0465325646cc6915d6f3f1e9d37a
Ashkan MV <ashkanmv@gmail.com>
2022-05-29 15:48:10 +0430

    salesmen location select inputs

--- src/app/pages/gps-tracking/gps-tracking.page.ts
+++ src/app/pages/gps-tracking/gps-tracking.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { PopoverController } from '@ionic/angular';
-import { PopoverComponent } from '../../component/popover/popover.component';
 
 @Component({
   selector: 'app-gps-tracking',
@@ -10,25 +8,10 @@ import { PopoverComponent } from '../../component/popover/popover.component';
 })
 export class GpsTrackingPage implements OnInit {
   Date: string = 'Date';
-  constructor(
-    private router: Router,
-    public popoverController: PopoverController
-  ) {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
   backButton() {
     this.router.navigate(['/']);
-  }
-  async presentPopover(ev: any) {
-    const popover = await this.popoverController.create({
-      component: PopoverComponent,
-      cssClass: 'my-custom-class',
-      event: ev,
-      translucent: true,
-    });
-    await popover.present();
-
-    const { role } = await popover.onDidDismiss();
-    console.log('onDidDismiss resolved with role', role);
   }
 }
