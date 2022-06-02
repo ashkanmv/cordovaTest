8a6b0f04f7866ec45b62ae08632a5a082bffef0c
Ashkan MV <ashkanmv@gmail.com>
2022-06-02 21:49:45 +0430

    search highlight removed/max tabel data

--- src/app/pages/max-pped/max-pped.page.ts
+++ src/app/pages/max-pped/max-pped.page.ts
@@ -11,29 +11,46 @@ export class MaxPPEDPage implements OnInit {
   dateNow = new Date();
   selectedSegment: string = 'per-customer';
 
-  routeData: {
-    route: number;
-    city: string;
-    visitorName: string;
-    sale: number;
-    PPED: number;
-    percentage: number;
-  };
-
   nestedTableIsShowingRow_1: boolean = false;
   nestedTableIsShowingRow_2: boolean = false;
   nestedTableIsShowingRow_3: boolean = false;
   nestedTableIsShowingRow_4: boolean = false;
-  constructor(private router: Router) {
-    this.routeData = {
+  constructor(private router: Router) {}
+
+  public routeData: Array<any> = [
+    {
       route: 1131,
       city: 'Tehran',
       visitorName: 'Behnam Ezadi',
       sale: 3.12,
       PPED: 19,
       percentage: 782,
-    };
-  }
+    },
+    {
+      route: 1132,
+      city: 'Tabriz',
+      visitorName: 'Behnaz Azadi',
+      sale: 4.12,
+      PPED: 9,
+      percentage: 7.82,
+    },
+    {
+      route: 1133,
+      city: 'Esfahan',
+      visitorName: 'Behzad Azari',
+      sale: 3.12,
+      PPED: 19,
+      percentage: 782,
+    },
+    {
+      route: 1134,
+      city: 'Amol',
+      visitorName: 'Behrooz Ezraee',
+      sale: 3.12,
+      PPED: 19,
+      percentage: 782,
+    },
+  ];
 
   ngOnInit() {}
   backButton() {
