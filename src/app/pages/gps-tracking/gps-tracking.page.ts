13c488a85e02cdfd5941e37150b3070cac38ffb9
a.mirzaei <ashkanmv@gmail.com>
2022-06-04 14:34:32 +0430

    GPS TRACKING

--- src/app/pages/gps-tracking/gps-tracking.page.ts
+++ src/app/pages/gps-tracking/gps-tracking.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Data, Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { MapService } from 'src/app/map/map.service';
+import { getUserCildrenResponse } from 'src/app/shared/common';
 import { PersianCalendarService } from 'src/app/shared/persian-calendar.service';
 import { StorageService } from 'src/app/shared/storage.service';
 
@@ -12,13 +13,14 @@ import { StorageService } from 'src/app/shared/storage.service';
   styleUrls: ['./gps-tracking.page.scss'],
 })
 export class GpsTrackingPage implements OnInit {
+  show = false;
   form: FormGroup;
   now_date = new Date().toISOString();
-  rsmList = [];
-  asms = [];
-  ssvs = [];
-  srs = [];
-
+  rsms: getUserCildrenResponse[] = [];
+  asms: getUserCildrenResponse[] = [];
+  ssvs: getUserCildrenResponse[] = [];
+  srs: getUserCildrenResponse[] = [];
+  routes: any[] = [];
   constructor(private router: Router,
     private formBuilder: FormBuilder,
     private persianCalendarService: PersianCalendarService,
@@ -33,17 +35,26 @@ export class GpsTrackingPage implements OnInit {
   }
 
   init() {
-    this.storageService.get('user_id').then((user_id) => {
+    this.storageService.get('user_id').then(user_id => {
       this.patchValue('userId', user_id);
     });
 
-    this.storageService.get('access').then((access) => {
+    this.storageService.get('access').then(access => {
       if (access)
         this.accessHandler(JSON.parse(access))
     })
 
     this.storageService.get('route_code').then(route_code => {
+      if (route_code)
+        this.patchValue('routeCode', route_code);
+    })
 
+    this.storageService.get('route_name').then(route_name => {
+      if (route_name)
+        this.patchValue('userRouteName', route_name);
+      if (this.f.selectedSr.value && this.f.userRouteName.value) {
+        this.getSrRouteAfterInit(this.f.selectedSr.value.id,this.f.selectedDate.value,this.f.userRouteName.value);
+      }
     })
   }
 
@@ -69,13 +80,14 @@ export class GpsTrackingPage implements OnInit {
       accessRsm: [false],
       accessAsm: [false],
       accessSsv: [false],
-      accessSr: [false]
+      accessSr: [false],
+      accessRoute: [true],
+      routeCode: [null],
+      userRouteName: [null],
     });
   }
 
-  get f() {
-    return this.form.controls;
-  }
+  get f() { return this.form.controls }
 
   accessHandler(accessJson: { name: string }[]) {
     var flg = false;
@@ -90,67 +102,119 @@ export class GpsTrackingPage implements OnInit {
           flg = true;
         }
       }
-      else if (access.name == 'gps_asm')
+      else if (access.name == 'gps_asm') {
         this.patchValue('accessAsm', true)
-      else if (access.name == 'gps_ssv')
+        if (flg == false) {
+          console.log('access');
+          this.patchValue('selectedAsm', this.f.userId.value)
+          this.asmSelect();
+          flg = true;
+        }
+      }
+      else if (access.name == 'gps_ssv') {
         this.patchValue('accessSsv', true)
-      else if (access.name == 'gps_sr')
+        if (flg == false) {
+          this.patchValue('selectedSsv', this.f.userId.value)
+          this.ssvSelect();
+          flg = true;
+        }
+      }
+      else if (access.name == 'gps_sr') {
         this.patchValue('accessSr', true)
+        if (flg == false) {
+          this.patchValue('selectedSr', this.f.userId.value)
+          this.srSelect();
+          flg = true;
+        }
+      }
       else if (access.name == 'gps_route')
-        this.patchValue('selectedRoute', true)
+        if (flg == false) {
+          this.patchValue('selectedRoute', this.f.userId.value)
+          this.routeSelect();
+          flg = true;
+        }
     });
   }
 
   rsmSelect() {
+    if (!this.f.selectedRsm.value)
+      return
     let selected_rsm = this.f.selectedRsm.value;
     if (selected_rsm.id) {
       this.patchValue('selectedAsm', selected_rsm.id)
-      this.asmSelect();
-      this.patchValue('selectedSsv', null)
+      // this.asmSelect();
+      // this.patchValue('selectedSsv', null)
       this.ssvs = [];
-      this.ssvSelect();
-      this.patchValue('selected_sr', null)
+      // this.ssvSelect();
+      this.patchValue('selectedSr', null)
       this.srs = [];
       // this.srSelect();
     } else
-      this.mapService.getUserCildren(selected_rsm).subscribe(res => this.rsmList = res)
+      this.mapService.getUserCildren(selected_rsm).subscribe(res => {
+        this.rsms = res
+        console.log('rsms', res);
+
+      })
   }
 
   asmSelect() {
-    let selected_asm = this.f.selectedRsm.value;
+    if (!this.f.selectedAsm.value)
+      return
+    let selected_asm = this.f.selectedAsm.value;
     if (selected_asm.id) {
       this.patchValue('selectedSsv', selected_asm.id)
-      this.ssvSelect();
-      this.patchValue('selected_sr', null)
+      // this.ssvSelect();
+      this.patchValue('selectedSr', null)
       this.srs = [];
       // this.srSelect();
     }
-    else {
-      this.mapService.getUserCildren(selected_asm).subscribe(res => this.asms = res);
-    }
+    else
+      this.mapService.getUserCildren(selected_asm).subscribe(res => {
+        this.asms = res
+        console.log('asms', res);
+      });
+
   }
 
   ssvSelect() {
+    if (!this.f.selectedSsv.value)
+      return
+    console.log(this.f.selectedSsv.value);
+
     let selected_ssv = this.f.selectedSsv.value;
     if (selected_ssv.id) {
       this.patchValue('selectedSr', selected_ssv.id)
-      this.sr_select();
+      // this.srSelect();
     }
     else {
       this.mapService.getUserCildren(selected_ssv).subscribe(res => this.ssvs = res);
     }
   }
 
-  sr_select() {
+  srSelect() {
     let selected_sr = this.f.selectedSr.value;
-    if (selected_sr.id != undefined) {
+    if (selected_sr.id) {
       this.patchValue('selectedRoute', selected_sr.id)
-      // this.route_select();
     }
     else {
       this.mapService.getUserCildren(selected_sr)
         .subscribe(values => this.srs = values)
     }
+  }
+
+  routeSelect() {
+    this.mapService.getSrRoute(this.f.selectedRoute.value, this.persianCalendarService.getVPTodayFormat(this.f.selectedDate.value)).subscribe((values: Data[]) => {
+      console.log(values);
+      this.routes = values;
+    })
+  }
+
+  getSrRouteAfterInit(selected_Route: string, selected_date: Date,user_route_name : string) {
+    this.mapService.getSrRoute(selected_Route, this.persianCalendarService.getTodayFormatEnd(selected_date))
+      .subscribe((values :any[]) => {
+        console.log(values);
+        this.srs = values;
+      })
   }
 
   patchValue(controller: string, value: any) {
