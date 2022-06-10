45c5fad42a161eed85c393f2ab7c56f2313555b9
Ashkan MV <ashkanmv@gmail.com>
2022-06-10 20:12:10 +0430

    tody planned mock data for table

--- src/app/pages/today-planned-not-buying/today-planned-not-buying.page.ts
+++ src/app/pages/today-planned-not-buying/today-planned-not-buying.page.ts
@@ -7,11 +7,58 @@ import { Router } from '@angular/router';
   styleUrls: ['./today-planned-not-buying.page.scss'],
 })
 export class TodayPlannedNotBuyingPage implements OnInit {
+  // mock data
+  public routeVisitorData: Array<any> = [
+    {
+      Route: 1100,
+      Visitor: 'Ali Mohammad',
+      count: 6,
+    },
+    {
+      Route: 1103,
+      Visitor: 'Mohammad Ali',
+      count: 0,
+    },
+    {
+      Route: 1104,
+      Visitor: 'Mohammad Mohamadian',
+      count: 2,
+    },
+    {
+      Route: 1105,
+      Visitor: 'Ali Alian',
+      count: 8,
+    },
+  ];
+  // --detail
+  public routeVisitorDetailData: Array<any> = [
+    {
+      Name: ' Lola Jahan',
+      Number: 205645654,
+      Address: 'Tehran_Zafar ',
+    },
+    {
+      Name: 'Jahan Zafaru',
+      Number: 657546456,
+      Address: 'Tehran_Zafar',
+    },
+    {
+      Name: 'Jahanan Esfahani',
+      Number: 46784784,
+      Address: 'Tehran_Zafar',
+    },
+    {
+      Name: 'Hanie Jahanian',
+      Number: 85674867845,
+      Address: 'Tehran_Zafar',
+    },
+  ];
+  //
   // delete later
   IsDetailsShowing = true;
   Is1DetailsShowing = false;
   //
-  constructor(private router: Router) {}
+  constructor() {}
   dateNow = new Date();
   ngOnInit() {}
   // delete later
