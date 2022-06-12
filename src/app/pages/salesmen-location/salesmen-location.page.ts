4894e6428d724e4f8904582e3b056006f2260b78
Ashkan MV <ashkanmv@gmail.com>
2022-06-13 00:23:39 +0430

    salesmanLocation start

--- src/app/pages/salesmen-location/salesmen-location.page.ts
+++ src/app/pages/salesmen-location/salesmen-location.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
 import { PopoverComponent } from 'src/app/shared/components/popover/popover.component';
+import { PersianCalendarService } from 'src/app/shared/persian-calendar.service';
+import { StorageService } from 'src/app/shared/storage.service';
 
 @Component({
   selector: 'app-salesmen-location',
@@ -10,10 +13,116 @@ import { PopoverComponent } from 'src/app/shared/components/popover/popover.comp
 })
 export class SalesmenLocationPage implements OnInit {
   dateNow = new Date();
+  show = false;
+  form: FormGroup;
 
-  constructor(private router: Router, public popoverctrl: PopoverController) {}
+  constructor(
+    private router: Router,
+    public popoverctrl: PopoverController,
+    private persianCalendarService: PersianCalendarService,
+    private formBuilder: FormBuilder,
+    private storageService: StorageService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getUserId();
+    this.loadForm();
+    this.checkAccess();
+  }
+
+  getUserId() {
+    this.storageService.get('user_id').then((user_id) => {
+      if (user_id) {
+        this.patchValue('userId', user_id);
+        this.patchValue('myUserID', user_id);
+      }
+    });
+  }
+
+  loadForm() {
+    this.form = this.formBuilder.group({
+      DC: [null],
+      Route: [null],
+      Customer: [null],
+      type: ['sales'],
+      kgqty: ['qty'],
+      showPointSd: [true],
+      showPointRsm: [true],
+      showPointAsm: [true],
+      showPointSsv: [true],
+      showPointSr: [true],
+      showTruck: [true],
+      showSr: [true],
+      showRsm: [true],
+      showAsm: [true],
+      showSsv: [true],
+      accessTime: [false],
+      accessRsm: [false],
+      myUserType: [null],
+      userId: [null],
+      myUserID: [null],
+      selectedDate: [new Date()],
+      formDate: [new Date().toISOString()],
+      srTime: [this.persianCalendarService.getVPTodayFormat(new Date())],
+      truckTime: [this.persianCalendarService.getTodayFormat(new Date())],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  checkAccess() {
+    this.storageService.get('access').then((access) => {
+      if (access) this.accessHandler(JSON.parse(access));
+    });
+  }
+
+  accessHandler(accessJson: { name: string }[]) {
+    var flg = false;
+    accessJson.forEach((access) => {
+      if (access.name == 'gps_dc') {
+        // this.access_dc = true;
+      } else if (access.name == 'gps_time') {
+        this.patchValue('accessTime', true);
+      } else if (access.name == 'gps_rsm') {
+        this.patchValue('accessRsm', true);
+        if (flg == false) {
+          this.patchValue('myUserType', 'rsm');
+          this.rsm_select();
+          flg = true;
+        }
+      } else if (access.name == 'gps_asm') {
+        this.patchValue('accessAsm', true);
+        if (flg == false) {
+          this.patchValue('myUserType', 'asm');
+
+          this.asm_select();
+
+          flg = true;
+        }
+      } else if (access.name == 'gps_ssv') {
+        this.patchValue('accessSsv', true);
+        if (flg == false) {
+          this.patchValue('myUserType', 'ssv');
+          this.ssv_select();
+          flg = true;
+        }
+      } else if (access.name == 'gps_sr') {
+        this.patchValue('accessSr', true);
+        if (flg == false) {
+          this.patchValue('myUserType', 'sr');
+          this.sr_select();
+          flg = true;
+        }
+      }
+    });
+  }
+
+  rsm_select() {}
+  asm_select() {}
+  ssv_select() {}
+  sr_select() {}
 
   async presentPopover(ev: any) {
     const popover = await this.popoverctrl.create({
@@ -26,5 +135,9 @@ export class SalesmenLocationPage implements OnInit {
 
     const { role } = await popover.onDidDismiss();
     console.log('onDidDismiss resolved with role', role);
+  }
+
+  patchValue(controller: string, value: any) {
+    this.form.patchValue({ [controller]: value });
   }
 }
