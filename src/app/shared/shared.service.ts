3ee783a10843ba2e016dbd7822766b828bbd8f45
a.mirzaei <ashkanmv@gmail.com>
2022-06-18 19:04:09 +0430

    x

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -6,7 +6,7 @@ import { PredefinedColors } from '@ionic/core';
   providedIn: 'root'
 })
 export class SharedService {
-
+  online = true;
   constructor(private toastCtrl: ToastController) { }
 
   toast(color: PredefinedColors, message: string) {
